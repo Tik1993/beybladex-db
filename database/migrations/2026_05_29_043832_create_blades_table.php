564031d96329d6fc995214ef8a8b42711fea278f
Ernest Lin <linchongtik@gmail.com>
2026-05-29 18:19:33 -0700

    fix blades migration

--- database/migrations/2026_05_29_043832_create_blades_table.php
+++ database/migrations/2026_05_29_043832_create_blades_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('blades', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('img_url')->nullable();
+            $table->string('color')->nullable();
             $table->timestamps();
         });
     }
