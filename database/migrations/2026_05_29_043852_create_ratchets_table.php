03f51954d6faa540645fff64a573b3304d6d9fa5
Ernest Lin <linchongtik@gmail.com>
2026-06-01 18:51:23 -0700

    update insert create_at in migration

--- database/migrations/2026_05_29_043852_create_ratchets_table.php
+++ database/migrations/2026_05_29_043852_create_ratchets_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -18,6 +19,25 @@ return new class extends Migration
             $table->string('color')->nullable();
             $table->timestamps();
         });
+
+        DB::unprepared('
+            CREATE OR REPLACE FUNCTION set_timestamps_on_insert()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                NEW.created_at = NOW();
+                NEW.updated_at = NOW();
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+        ');
+
+        DB::unprepared('
+            DROP TRIGGER IF EXISTS set_timestamps_on_insert_ratchets ON ratchets;
+            CREATE TRIGGER set_timestamps_on_insert_ratchets
+            BEFORE INSERT ON ratchets
+            FOR EACH ROW
+            EXECUTE FUNCTION set_timestamps_on_insert();
+        ');
     }
 
     /**
--- database/migrations/2026_05_30_222515_create_official_setups_table.php
+++ database/migrations/2026_05_30_222515_create_official_setups_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -28,6 +29,25 @@ return new class extends Migration
 
             $table->timestamps();
         });
+
+        DB::unprepared('
+            CREATE OR REPLACE FUNCTION set_timestamps_on_insert()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                NEW.created_at = NOW();
+                NEW.updated_at = NOW();
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+        ');
+
+        DB::unprepared('
+            DROP TRIGGER IF EXISTS set_timestamps_on_insert_official_setups ON official_setups;
+            CREATE TRIGGER set_timestamps_on_insert_official_setups
+            BEFORE INSERT ON official_setups
+            FOR EACH ROW
+            EXECUTE FUNCTION set_timestamps_on_insert();
+        ');
     }
 
     /**
